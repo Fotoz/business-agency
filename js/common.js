ab7bb0c6c5488d8c8eeed56b4832ea1e5da04029
Fotoz <fotozxx@gmail.com>
2019-07-13 17:10:15 +0300

    full-desktop

--- js/common.js
+++ js/common.js
@@ -1,5 +1,31 @@
 $(function() {
 
-	// Custom JS
+// Smooth-scrolling
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
+// toTop button
+$(window).scroll(function() {
+	if ($(this).scrollTop() > $(this).height()) {
+			$('.toTop').addClass('toTop-active');
+	} else {
+			$('.toTop').removeClass('toTop-active');
+	};
+});
+$('.toTop').click(function() {
+	$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
 
 });
